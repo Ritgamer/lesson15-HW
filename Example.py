c1cd338ce1549c52f183f9937f10c49fbf4c1304
Ritgamer <masterrithin@gmail.com>
2025-06-02 10:19:17 +0530

    yea i done it

---
+++ Example.py
@@ -0,0 +1,20 @@
+import math
+
+print("math.fsum")
+
+print(math.fsum([1.7, 0.3, 1.5, 4.5]))
+
+
+print("math.log")
+
+print(math.log(2.7183))
+
+
+print("math.cos")
+
+print (math.cos(10))
+
+
+print("math.degrees")
+
+print (math.degrees(90))
